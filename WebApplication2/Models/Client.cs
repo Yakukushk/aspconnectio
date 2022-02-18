f2b4eca41ed403d1b7ba5bd0a6971baa75c21f01
Yakukushk <daniilliubko@gmail.com>
2022-02-18 19:36:11 +0200

    First Commit

---
+++ WebApplication2/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication2
+{
+    public partial class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Payment { get; set; } = null!;
+        public string Location { get; set; } = null!;
+
+        public virtual TourClient IdNavigation { get; set; } = null!;
+    }
+}
